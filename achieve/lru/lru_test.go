51f72d4f553771bbee46bd45ba3220a01eb2cd30
geraldkohn <dysonconrad@outlook.com>
2022-10-17 09:56:25 +0000

    归档

--- achieve/lru/lru_test.go
+++ achieve/lru/lru_test.go
@@ -1,10 +1,8 @@
-package lru_test
+package lru
 
 import (
 	"reflect"
 	"testing"
-
-	"mini-cache/lru"
 )
 
 type String string
@@ -14,7 +12,7 @@ func (s String) Len() int {
 }
 
 func TestGet(t *testing.T) {
-	lru := lru.New(int64(0), nil)
+	lru := New(int64(0), nil)
 	lru.Add("key1", String("1234"))
 	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
 		t.Fatalf("cache hit key1=1234 failed")
@@ -28,7 +26,7 @@ func TestRemoveOldest(t *testing.T) {
 	k1, k2, k3 := "key1", "key2", "k3"
 	v1, v2, v3 := "value1", "value2", "v3"
 	cap := len(k1 + k2 + v1 + v2)
-	lru := lru.New(int64(cap), nil)
+	lru := New(int64(cap), nil)
 	lru.Add(k1, String(v1))
 	lru.Add(k2, String(v2))
 	lru.Add(k3, String(v3))
@@ -40,10 +38,10 @@ func TestRemoveOldest(t *testing.T) {
 
 func TestOnEvicted(t *testing.T) {
 	keys := make([]string, 0)
-	callback := func(key string, value lru.Value) {
+	callback := func(key string, value Value) {
 		keys = append(keys, key)
 	}
-	lru := lru.New(int64(10), callback)
+	lru := New(int64(10), callback)
 	lru.Add("key1", String("123456"))
 	lru.Add("k2", String("k2"))
 	lru.Add("k3", String("k3"))
